1e3cd4e82a71877c914a30198a4ec3471284645b
Jorge Luis Mozo castro <156432629+jorgy2910JD@users.noreply.github.com>
2024-07-24 09:10:58 -0500

    feat :construction: software Hospital

---
+++ Dia_3/src/main/java/com/mycompany/javasql/Hospital.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javasql;
+
+/**
+ *
+ * @author camper
+ */
+public class Hospital {
+    private String nombre;
+    
+// Constructor 
+    public Hospital (String nombre){
+        this.nombre = nombre;
+    }
+    
+// getter para el dato
+    public String getNombre () {
+        return nombre;
+    }
+}
+
